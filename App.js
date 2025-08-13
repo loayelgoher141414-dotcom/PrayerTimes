70287449a7955e368cfa1c1709968c77083aea70
loayelgoher141414-dotcom <loayelgoher141414@gmail.com>
2025-08-13 14:58:49 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './output.css';
+import Home from "./Dash/Home";
+import Card from "./Dash/Head";
+import Base from "./Dash/base";
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Card/>
+      <Base/>
+    </div>
+  );
+}
+export default App;
+
+
+
+
+
